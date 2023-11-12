dc6972bcef632c10e0b888690c8f4da581eaf587
Suraj Kumar <surajbansikumar@gmail.com>
2023-11-12 16:34:18 +0530

    main.html added

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -12,3 +12,16 @@ def members(request):
     }
     return HttpResponse(template.render(context, request))
     #return HttpResponse("Hello world from Member View!")
+
+
+def main(request):
+  template = loader.get_template('main.html')
+  return HttpResponse(template.render())
+
+def details(request, id):
+  mymember = Member.objects.get(id=id)
+  template = loader.get_template('details.html')
+  context = {
+    'mymember': mymember,
+  }
+  return HttpResponse(template.render(context, request))
